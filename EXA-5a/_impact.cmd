1647e2a28c43115d07113644dcca2c56fba83538
bandekar <90973610+sanbancan@users.noreply.github.com>
2023-12-20 14:17:00 -0800

    Add files via upload

---
+++ EXA-5a/_impact.cmd
@@ -0,0 +1,13 @@
+setPreference -pref AutoSignature:FALSE
+setPreference -pref KeepSVF:FALSE
+setPreference -pref ConcurrentMode:FALSE
+setPreference -pref UseHighz:FALSE
+setPreference -pref UserLevel:NOVICE
+setPreference -pref svfUseTime:FALSE
+setMode -bs
+setMode -bs
+setCable -port auto
+Identify 
+identifyMPM 
+assignFile -p 1 -file "E:/detop/PSTYRO-FPGASP3/CODE/EXA-5a/object_co.bit"
+Program -p 1 -defaultVersion 0 
